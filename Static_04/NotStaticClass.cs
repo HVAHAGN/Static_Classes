677d279e4140a633e3c66c5fe72b25db5d9035ab
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-30 23:59:19 +0400

    From static members cant call not static member

---
+++ Static_04/NotStaticClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_04
+{
+    class NotStaticClass
+    {
+        //static field default private
+        static int field;
+
+        //static property Field
+        //from static members cant call not static member
+
+        public static int Field
+        {
+            get { return field; }
+            set { field = value; }
+
+
+
+        }
+    }
+}
---
+++ Static_04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NotStaticClass.Field = 33;
+            Console.WriteLine(NotStaticClass.Field);
+            Console.ReadLine();
+        }
+    }
+}
