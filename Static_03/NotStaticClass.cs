2e300a71140aa510db13c9601f92bdf62fd55de5
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-30 23:42:13 +0400

    Constant member of class

---
+++ Static_03/NotStaticClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_03
+{
+    class NotStaticClass
+    {
+        //Constant cant be static because it's behavior is already static
+        //public static const float e = 2.174586987896;
+        // field const relates to type not an instance
+        // you can call const field as static member of class
+        public const float e = 2.17545869874333f;
+
+
+    }
+}
---
+++ Static_03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("e={0}", NotStaticClass.e);
+
+            Console.ReadLine();
+        }
+    }
+}
