64d4b82843b9c8872eb84f42d6318b256ccdac97
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-31 02:51:01 +0400

    Singleton pattern

---
+++ Singleton_10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Protected constructor cant used
+            Singleton instance1 = Singleton.Instance();
+            Singleton instance2 = Singleton.Instance();
+
+            if(instance1==instance2)
+            {
+                Console.WriteLine("Referenced to the same instance");
+            }
+            Console.WriteLine(instance1.GetHashCode());
+            Console.WriteLine(instance1.GetHashCode());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton_10/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_10
+{ //Singleton
+    class Singleton
+    {
+        private static Singleton instance = null;
+        //Constructor protected
+        protected Singleton()
+        {
+
+        }
+        //Factory method
+        public static Singleton Instance()
+        { //if instance not created
+            if(instance==null)
+
+            {//then create an instance
+                instance = new Singleton();
+            }
+            //else return link an existing instance
+            return instance;
+        }
+
+    }
+}
