3b114417b3a4472eebb52af419be1f4f09378d58
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-30 23:26:13 +0400

    Cant call not static member from static method

---
+++ Static_02/NotStaticClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_02
+{
+    class NotStaticClass
+    {
+        private int Id;
+        //Constructor
+        public NotStaticClass(int id)
+        {
+            this.Id = id;
+
+        }
+        public static void Method()
+        {
+           // Console.WriteLine("Instance.Id={0}", Id);
+            
+            Console.WriteLine("We cant call not static member from static method ");
+        }
+
+    }
+}
---
+++ Static_02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NotStaticClass instance = new NotStaticClass(1);
+            NotStaticClass.Method();
+
+            Console.ReadLine();
+        }
+    }
+}
