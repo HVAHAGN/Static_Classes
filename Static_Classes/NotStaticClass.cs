cb7172b0c48c1c0046d935c0c3ab4efd4497e4d9
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-30 23:07:29 +0400

    Add value to static field

---
+++ Static_Classes/NotStaticClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Classes
+{
+    class NotStaticClass
+    {
+        private int Id;
+        public static int field;
+
+        public NotStaticClass(int id)
+        {
+            this.Id = id;
+
+        }
+        public void Method()
+        {
+            // We can call from not static method a static member of class 
+            Console.WriteLine("Instance {0}.field={1}", Id, field);
+        }
+
+    }
+}
--- Static_Classes/Program.cs
+++ Static_Classes/Program.cs
@@ -10,6 +10,16 @@ namespace Static_Classes
     {
         static void Main(string[] args)
         {
+            NotStaticClass instance1 = new NotStaticClass(1);
+            NotStaticClass instance2 = new NotStaticClass(2);
+            instance1.Method();
+            instance2.Method();
+
+            // Notstatic class-object call
+            NotStaticClass.field = 1;
+            instance1.Method();
+            instance2.Method();
+
             Console.ReadLine();
         }
     }
