80eb3ccd5317e71e7d3b4bd51d69e05b03052e93
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-31 01:15:00 +0400

    If Class has static constructor it call the first

---
+++ Constructors_07/NotStaticClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_07
+{
+    class NotStaticClass
+    {
+        // Instance constructor, called only when creating an instance of a class.
+        public NotStaticClass()
+        {
+            Console.WriteLine("Constructor.");
+        }
+
+        // The static constructor is called anyway
+        // as when referring to a static member, and when creating an instance of the class.
+
+        static NotStaticClass()
+        {
+            Console.WriteLine("Static constructor");
+        }
+            
+        public static void StaticMethod()
+        {
+            Console.WriteLine("Static Method");
+        }
+            //Not static method
+        public void NotStaticMethod()
+        {
+            Console.WriteLine("Notstatic method");
+        }
+        
+    }
+}
+
---
+++ Constructors_07/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Is Called only static constructor
+           // NotStaticClass.StaticMethod();
+
+            // both constructors called
+
+            new NotStaticClass().NotStaticMethod();
+
+            Console.ReadLine();
+        }
+    }
+}
